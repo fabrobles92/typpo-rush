e34038ae4528ff9786b4572bd895b32c8cff5c2d
Fabricio <fabrobles92@gmail.com>
2024-09-05 17:45:06 -0600

    Update License Include Optimism Sepolia

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,12 +1,12 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { optimism, sepolia } from "wagmi/chains";
+import { optimism, optimismSepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
   appName: "Typpo Rush",
   projectId: "66ea8e233ab5eaeb4026c8d8da37739e",
   chains: [
     optimism,
-    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [optimismSepolia] : []),
   ],
   ssr: true,
 });
